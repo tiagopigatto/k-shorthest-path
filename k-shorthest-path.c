cd2d7405e7ea8624ff11bdf88f03db1e59efe502
Tiago Pigatto <tiago.sk82@gmail.com>
2015-08-27 07:58:44 -0700

    Initial

---
+++ k-shorthest-path.c
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+
+void dijkstra(int size, int **matriz, int no1, int no2, int custos, int caminho);
+
+int main(int argc, const char *argv[]){
+
+
+	return 0;
+}
+
+
+void dijkstra(int size, int **matriz, int no1, int no2, int custos, int caminho){
+
+}
