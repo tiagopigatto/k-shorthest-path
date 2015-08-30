3f7e533bb65e87ce09ec8f5e6d04ffdb32abf4f2
Richard <richardmcr@gmail.com>
2015-08-30 14:57:43 -0700

    menor custo

--- k-shorthest-path.cpp
+++ k-shorthest-path.cpp
@@ -3,17 +3,18 @@
 #include<string.h>
 #include <vector>
 #include <iostream>
+#include <algorithm>
+#include <iterator>
 
+using namespace std;
 
-void dijkstra(int size, int **matriz, int no1, int no2, vector<int>& custos, vector<int& caminho>);
+void dijkstra(int size, int **matriz, int no1, int no2, vector<int>& custos, vector<int>& caminho);
 int leTopologia(FILE *p, int **matriz);
 int menorCusto(vector<int>& custos);
 void atualizaMatriz(int menorCusto, int **matriz, vector<int>&);
 
 
 int main(int argc, const char *argv[]){
-
-
 
 	return 0;
 }
@@ -28,11 +29,13 @@ void dijkstra(int size, int **matriz, int no1, int no2, vector<int>& custos, vec
 }
 
 int menorCusto(vector<int>& custos){
-
-
+	int menor = *min_element(custos.begin(), custos.end());
+	int indice  = distance(custos.begin(), find(custos.begin(), custos.end(), menor));
+	return indice;
 }
 
 void atualizaMatriz(int menorCusto, int **matriz, vector<int>&){
 
 
 }
+
