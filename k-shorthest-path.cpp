224e67b0a48985115e139b49065acc23e43da98f
Tiago Pigatto <tiago.sk82@gmail.com>
2015-08-30 13:05:02 -0700

    Funcoes iniciais

---
+++ k-shorthest-path.cpp
@@ -0,0 +1,38 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include <vector>
+#include <iostream>
+
+
+void dijkstra(int size, int **matriz, int no1, int no2, vector<int>& custos, vector<int& caminho>);
+int leTopologia(FILE *p, int **matriz);
+int menorCusto(vector<int>& custos);
+void atualizaMatriz(int menorCusto, int **matriz, vector<int>&);
+
+
+int main(int argc, const char *argv[]){
+
+
+
+	return 0;
+}
+
+int leTopologia(FILE *p, int **matriz){
+
+}
+
+
+void dijkstra(int size, int **matriz, int no1, int no2, vector<int>& custos, vector<int>& caminho){
+
+}
+
+int menorCusto(vector<int>& custos){
+
+
+}
+
+void atualizaMatriz(int menorCusto, int **matriz, vector<int>&){
+
+
+}
